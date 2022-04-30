540b83b299d5d48aac8c454f56f529d7a702c4d3
Daniel Sánchez <ingdanielsanchezve@gmail.com>
2022-04-30 19:47:32 +0200

    First commit to heroku

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express'),
       routes = require('./routes'),
       bodyParser = require('body-parser'),
       logger = require('morgan')
+      cors = require('cors')
 
 const app = express()
+
+app.use( cors({ credentials: true, origin: true }) )
+app.options('*', cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
